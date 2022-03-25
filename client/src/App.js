a73351ceb261bab2d85e04a86c6214fea487697e
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2022-03-26 01:53:44 +0530

    :construction: added theme context

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { ThemeChangeProvider } from "./contexts/ThemeChangeContext";
+import MainComponent from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
 
+// App
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeChangeProvider>
+      <BrowserRouter basename="/">
+        <MainComponent />
+      </BrowserRouter>
+    </ThemeChangeProvider>
   );
 }
 
