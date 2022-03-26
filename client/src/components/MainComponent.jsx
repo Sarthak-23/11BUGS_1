d1ef40897973df8aa1fd71b218fcb4a0c660c9e6
Sarthak-23 <jainsarthak0786@gmail.com>
2022-03-26 12:45:11 +0530

    Completed the Authentication for server and client

--- client/src/components/MainComponent.jsx
+++ client/src/components/MainComponent.jsx
@@ -2,13 +2,35 @@ import { useContext } from "react";
 import { ThemeChangeContext } from "../contexts/ThemeChangeContext";
 import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./Home/Home";
 import Auth from "./Auth/Auth";
 import Profile from "./Profile/Profile";
+import { UserContext } from "../contexts/UserContext";
+import Login from "./Auth/Login";
 
 const MainComponent = () => {
   const [mode] = useContext(ThemeChangeContext);
+  const [user, setUser] = useContext(UserContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchUser = async () => {
+      let res = await fetch("/auth/user");
+      res = await res.json();
+      return res;
+    };
+    fetchUser().then((res) => {
+      if (!res.error) {
+        setUser(() => {
+          return {
+            ...res,
+            id: res.id,
+          };
+        });
+        navigate("/");
+      }
+    });
+  }, []);
 
   //   Theme
   const theme = createTheme({
@@ -20,9 +42,10 @@ const MainComponent = () => {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" component={<Home />} />
-        <Route path="/profile/:id" component={<Profile />} />
-        <Route path="/auth" component={<Auth />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/auth/signin" element={<Login />} />
       </Routes>
     </ThemeProvider>
   );
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const db = require("./config/db_config");
+const cookieParser = require("cookie-parser");
+
+// Routers
 const home = require("./routes/home");
 const auth = require("./routes/auth");
 const profile = require("./routes/profile");
@@ -10,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 // App
 const app = express();
+
+//MiddleWares
+app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/", home);
